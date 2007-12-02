5b3d51b78b07d2539a0fd38276129252816d4c9a
ma_boehm <ma_boehm@84247619-5e4a-0410-ba8a-c2b51db7ef0e>
2007-12-02 12:24:31 +0000

    first version uploaded to sourceforge

---
+++ trunk/dupfinder/buildunix.sh
@@ -0,0 +1,4 @@
+cd /cygdrive/c/programs/cpp/dbl
+g++ -o dblunix.exe dbl.cpp os_cc_specific.cpp
+#g++ -o dblunix0.exe -DBENCHMARK -DTESTFUNC0 dbl.cpp
+#g++ -o dblunix4.exe -DBENCHMARK -DTESTFUNC0 -DTESTCNT0 dbl.cpp
---
+++ trunk/dupfinder/dbl.h
@@ -0,0 +1,36 @@
+
+#ifndef __DBL_H_123
+#define __DBL_H_123
+
+#define BASEBUFSIZE 512
+
+struct fileinfo
+{
+	char name[MAX_PATH];
+	ULARGE_INTEGER size;
+	int nFirstBytes;
+	int nMaxFirstBytes;
+	char *firstbytes;
+	FileHandle fh;
+};
+
+struct findfileinfo
+{
+	ULARGE_INTEGER nMaxFileSizeIgnore;
+	list<fileinfo> *pFiles;
+};
+
+struct fileinfoequal
+{
+	list<fileinfo> files;
+};
+
+
+struct fileinfosize
+{
+	ULARGE_INTEGER size;
+	list<fileinfo> files;
+	list<fileinfoequal> equalfiles;
+};
+#endif /* defined(__DBL_H_123) */
+
