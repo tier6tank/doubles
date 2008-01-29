c0561e448ec2fdd0dfba4ce41151ca8ed7ac6d03
nobody <nobody@84247619-5e4a-0410-ba8a-c2b51db7ef0e>
2008-01-29 14:36:23 +0000

    This commit was manufactured by cvs2svn to create tag 'dupfinder-gui-0_10-release'.

---
+++ tags/dupfinder-gui-0_10-release/dupfinder/dialog3_tree.h
@@ -0,0 +1,64 @@
+/*
+ *  dupfinder - search for duplicate files
+ *  Copyright (C) 2007-2008 Matthias Boehm
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef DIALOG_3_H
+#define DIALOG_3_H
+
+#include "stdinc.h"
+using namespace std;
+#include "dbl.h"
+
+class DupFinderDlg3 : public wxDialog
+{
+public:
+	DupFinderDlg3(wxWindow *, findfileinfo &);
+	
+	~DupFinderDlg3();
+
+	void OnClose(wxCloseEvent &);
+	void OnSize(wxSizeEvent &);
+
+	void OnStore(wxCommandEvent &);
+
+	void CreateControls();
+	void DisplayResults();
+
+	void OnInitDialog(wxInitDialogEvent &);
+
+private:
+
+	findfileinfo &ffi;
+
+	wxListView *wResultList;
+	wxCheckBox *wReverse;
+
+	wxLog *oldlogtarget;
+
+	DECLARE_EVENT_TABLE()
+};
+
+enum {
+	ID_RESULTLIST = 1, 
+	ID_REVERSE, 
+	ID_STORE
+};
+
+
+#endif
