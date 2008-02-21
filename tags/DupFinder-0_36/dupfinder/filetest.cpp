4e3235555df8951963c7f7533d16aeb42a53a37c
nobody <nobody@84247619-5e4a-0410-ba8a-c2b51db7ef0e>
2008-02-21 15:17:19 +0000

    This commit was manufactured by cvs2svn to create tag 'DupFinder-0_36'.

---
+++ tags/DupFinder-0_36/dupfinder/filetest.cpp
@@ -0,0 +1,177 @@
+/******************************************************************************
+    Duplicate Files Finder - search for duplicate files
+    Copyright (C) 2007-2008 Matthias Boehm
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+******************************************************************************/
+
+#include "stdinc.h"
+#include "filetest.h"
+#include "dbl.h"
+
+const int FileTest::BUFSIZE = BASEBUFSIZE << 7;
+
+FileTest::FileTest(){
+	init();
+}
+
+FileTest::FileTest(const wxString &_name) {
+	init();
+	data->name = _name;
+}
+
+void FileTest::init() {
+	data = new FileTestdata;
+	data->extdata = NULL;
+	data->rcount = 1;
+}
+
+FileTest::~FileTest() {
+	ReleaseData();
+}
+
+FileTest::FileTest(const FileTest & rhs) {
+	data = rhs.data;
+	data->rcount++;
+}
+
+FileTest & FileTest::operator = (const FileTest &rhs) {
+	if(&rhs != this) {
+		ReleaseData();
+		data = rhs.data;
+		data->rcount++;
+	}
+	return *this;
+}
+
+void FileTest::ReleaseData() {
+	assert(data);
+	assert(data->rcount >= 1);
+
+	data->rcount--;
+
+	if(data->rcount == 0) {
+		// delete object
+
+		ReleaseExtData();
+
+		delete data;
+	}
+}
+
+bool FileTest::Open() {
+	bool bResult;
+
+	if(!data->extdata) {
+		data->extdata = new extFileTestdata;
+
+		bResult = data->extdata->FileTest.Open(data->name);
+	}
+	else {
+		bResult = data->extdata->FileTest.IsOpened();
+	}
+	return bResult;
+}
+
+
+bool FileTest::Read(char *buffer, int &ncount) {
+	assert(data->extdata);
+
+	assert(ncount == FileTest::BUFSIZE);
+
+	if(!data->extdata->FileTest.IsOpened()) {
+		return false;
+	}
+
+	ncount = data->extdata->FileTest.Read(buffer, ncount);
+
+	return ncount != wxInvalidOffset;
+}
+
+void FileTest::Close() {
+	ReleaseExtData();
+	init();
+}
+
+void FileTest::ReleaseExtData()
+{
+	if(data->extdata) {
+		if(data->extdata->FileTest.IsOpened()) {
+			data->extdata->FileTest.Close();
+		}
+		delete data->extdata;
+	}
+}
+
+bool	comparefiles0(File &_f1, File &_f2) {
+	bool bResult;
+	static char *b1, *b2;
+	if(b1 == NULL) { b1 = new char[FileTest::GetBufSize()]; }
+	if(b2 == NULL) { b2 = new char[FileTest::GetBufSize()]; }
+	int BUFSIZE = FileTest::GetBufSize();
+	int n1, n2;
+	FileTest f1, f2;
+	f1 = _f1; f2 = _f2;
+
+	if(!f1.Open()) {
+		return false;
+	}
+	if(!f2.Open()) {
+		return false;
+	}
+
+	while(1) {
+		n1 = n2 = BUFSIZE;
+		bool br1, br2;
+
+		br1 = f1.Read(b1, n1);
+		br2 = f2.Read(b2, n2);
+
+#ifdef BENCHMARK
+		__nBytesRead += n1;
+		__nBytesRead += n2;
+		__nSectorsRead += n1/BASEBUFSIZE + (n1%BASEBUFSIZE != 0 ? 1 : 0);
+		__nSectorsRead += n2/BASEBUFSIZE + (n2%BASEBUFSIZE != 0 ? 1 : 0);
+#endif
+
+		if(n1 != n2 || !br1 || !br2) {
+			bResult = false;
+			goto End;
+		}
+
+		if(memcmp(b1, b2, n1) != 0) {
+			bResult = false;
+			goto End;
+		}
+
+		if(n1 < BUFSIZE)
+			break;
+
+	}
+
+	f1.Close();
+	f2.Close();
+
+	bResult = true;
+
+End:	return bResult;
+}
+
+
+
+
+
+
---
+++ tags/DupFinder-0_36/dupfinder/readme.txt
@@ -0,0 +1,105 @@
+
+THE PROGRAM: 
+------------
+
+The program searches for duplicate files. Because it does not use any hashing algorithm, 
+it is much faster than other programs. There are both a gui and command line version available. 
+
+
+INSTALLATION:
+-------------
+
+As currently there is no installation program, you have to extract the files of the program in 
+a folder you want. From there, you can either execute the command line version (dupf) in a console
+or the gui version (dupfgui) by double clicking at the symbol in the file explorer. 
+
+
+USAGE:
+-------
+
+Commandline version:
+====================
+
+Type dupf for getting hints for the command line. 
+Here a summary: Simply type dupf <path1> <path2> <...> on the command line, for searching 
+in the paths path1 and path2 and ... for duplicates. 
+You can specify one or more options additionally. 
+
+Graphical User interface version:
+=================================
+
+First add the paths in which you want to search in to the list by clicking on Add, 
+and specify options as you want. 
+Then click on Go! and wait, until the results page is displayed. 
+
+COMPILING:
+----------
+
+1) You have to have the wxWidgets library installed. 
+   To get there, you have to download wxWidgets from 
+   http://sourceforge.net/projects/wxwindows
+
+a) In Unix configure the whole library with options as you like 
+   (but --disable-shared is required, you could but also change 
+    the makefiles to be able to link with shared libraries), 
+   for example
+   configure --with-gtk --disable-shared [--enable-debug]
+
+b) In Windows, follow the instructions you find in the docs directory
+
+
+2) The rest is pretty simple: 
+
+For each compiler, there is a special makefile. 
+In unix, there is also a (still experimental) autotools implementation.
+
+
+For building the program with visual c++ - compiler, just type the following: 
+
+	nmake -f makefile.vc [debug=0/1] [unicode=0/1]
+
+The compiled files are stored under vc[u][d]. 
+
+
+For building the program with borland c++, type the following:
+
+	make -f makefile.bcc [-Ddebug=0/1] [-Dunicode=0/1]
+
+The compiled files are stored under bcc[u][d]. 
+
+
+For building the program with mingw, type the following (on the windows 
+command line, not on msys or another unix emulation shell!):
+
+	mingw32-make -f makefile.gcc [debug=0/1] [unicode=0/1]
+
+The compiled files are stored under gcc[u][d]. 
+
+
+If you don't have mingw but only cygwin, you can build the program that way
+(note that you have to follow the steps for unix in 1a first using --with-msw instead
+of --with-gtk or --with-motif):
+
+	make cygwin=1 [debug=0/1]
+
+The compiled files are stored under unix[d]. 
+
+
+For building the program in unix with gcc, type the following:
+
+	make [debug=0/1]
+
+The compiled files are stored under unix[d].
+Or take the configure way (but note that this is still experimental):
+
+	./configure
+	make -f Makefile
+ 
+
+Other compilers are perhaps also supported, but i did not try other compilers yet. 
+
+
+LICENSE:
+-------
+See license.txt
+
