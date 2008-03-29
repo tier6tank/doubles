ef20f30641c0e0dcd02688ebf533fed4f6d0e361
nobody <nobody@84247619-5e4a-0410-ba8a-c2b51db7ef0e>
2008-03-29 08:30:15 +0000

    This commit was manufactured by cvs2svn to create tag 'DupFinder-0_42'.

---
+++ tags/DupFinder-0_42/dupfinder/stdinc.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+    Duplicate Files Finder - search for duplicate files
+    Copyright (C) 2007-2008 Matthias Boehm
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+******************************************************************************/
+
+#ifndef __STDINC_H_010987
+#define __STDINC_H_010987
+
+#include <wx/wx.h>
+#include <wx/dir.h>
+#include <wx/filename.h>
+#include <wx/file.h>
+#include <wx/log.h>
+#include <wx/listctrl.h>
+#include <wx/aboutdlg.h>
+#include <wx/clipbrd.h>
+#include <wx/treectrl.h>
+#include <cstdio>
+#include <cassert>
+#include <ctime>
+#include <cmath>
+#include <list>
+#include <set>
+#include "minmax.h"
+#include "os_cc_specific_includes.h"
+
+using namespace std;
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+
+#endif
+
